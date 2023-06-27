6dc5b91d92dabf5f055b68f413285c6ca8b0a200
trongxt-0611 <trongga9a@gmail.com>
2023-06-27 15:54:19 +0700

    update nginx config

--- run.py
+++ run.py
@@ -3,3 +3,4 @@ from mysocket import create_app, socketio
 app = create_app()
 # 192.168.0.14
 socketio.run(app, host="localhost", port=8080)
+# sudo nohup /home/ubuntu/FlashCardAI/venv/bin/python3 run.py > log.txt 2>&1 &
